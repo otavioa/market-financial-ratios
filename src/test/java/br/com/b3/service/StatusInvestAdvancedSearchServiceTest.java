59eac9ee0cd8d5c99feeecc9fa2419ee30ea5015
Otávio <otavio.junior@objective.com.br>
2021-11-01 14:20:35 -0300

    inicio novos testes

--- src/test/java/br/com/b3/service/StatusInvestAdvancedSearchServiceTest.java
+++ src/test/java/br/com/b3/service/StatusInvestAdvancedSearchServiceTest.java
@@ -1,11 +1,22 @@
 package br.com.b3.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.b3.service.dto.AdvanceSearchResponse;
+
+@ExtendWith(MockitoExtension.class)
 class StatusInvestAdvancedSearchServiceTest {
 
+	@InjectMocks
+	private StatusInvestAdvancedSearchService subject;
+	
 	@Test
-	void test() {
+	void getAllAcoes() {
+		
+		AdvanceSearchResponse allAcoes = subject.getAllAcoes();
 	}
 
 }
