0b52e372177515f971e5200b1addbe6356a14971
Otávio Augusto Jr <otavio.augusto.jr@gmail.com>
2021-11-04 00:09:00 -0300

    correção segurança

--- src/main/java/br/com/b3/controller/PagesController.java
+++ src/main/java/br/com/b3/controller/PagesController.java
@@ -1,13 +1,14 @@
 package br.com.b3.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class PagesController {
 
-	@RequestMapping
+	@GetMapping
 	public String home() {
 		return "home";
 	}
