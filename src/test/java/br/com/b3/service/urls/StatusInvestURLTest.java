3925bef02f37eda37e9674bbe80a3b520e1a290e
Otávio Augusto Jr <otavio.augusto.jr@gmail.com>
2021-11-14 15:56:28 -0300

    implementação de testes para StatusInvestURL

---
+++ src/test/java/br/com/b3/service/urls/StatusInvestURLTest.java
@@ -0,0 +1,49 @@
+package br.com.b3.service.urls;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class StatusInvestURLTest {
+
+	@BeforeEach
+	public void setUpEnvironment() {
+		StatusInvestURL.setUrl(null);
+	}
+	
+	@Test
+	void getDefaultURL() {
+		String url = StatusInvestURL.getUrl();
+		
+		Assertions.assertThat(url).isEqualTo("https://statusinvest.com.br/{categoria}/{ticket}");
+	}
+	
+	@Test
+	void getAdvancedDefaultURL() {
+		String url = StatusInvestAdvanceSearchURL.getUrl();
+		
+		Assertions.assertThat(url).isEqualTo("https://statusinvest.com.br/category/advancedsearchresult?search={search}&CategoryType={categoryType}");
+	}
+	
+	@Test
+	void getDefaultURLForTest() {
+		StatusInvestURL.setUrl("urlTest");
+		
+		String url = StatusInvestURL.getUrl();
+		
+		Assertions.assertThat(url).isEqualTo("urlTest");
+	}
+	
+	@Test
+	void getAdvancedURLForTest() {
+		StatusInvestAdvanceSearchURL.setUrl("urlTest");
+		
+		String url = StatusInvestAdvanceSearchURL.getUrl();
+		
+		Assertions.assertThat(url).isEqualTo("urlTest");
+	}
+
+}
