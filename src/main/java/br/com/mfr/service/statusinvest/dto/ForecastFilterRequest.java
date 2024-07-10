0e8e0d69bf20e881adae4d2f571475e2fb9d8a25
otavioa <otavio.augusto.jr@gmail.com>
2024-07-10 13:06:25 -0300

    - Fix warning and remove primitive array

--- src/main/java/br/com/mfr/service/statusinvest/dto/ForecastFilterRequest.java
+++ src/main/java/br/com/mfr/service/statusinvest/dto/ForecastFilterRequest.java
@@ -1,16 +1,18 @@
 package br.com.mfr.service.statusinvest.dto;
 
+import java.util.List;
+
 public record ForecastFilterRequest(
 		SubFilter upsidedownside,
 		SubFilter estimatesnumber,
 		Boolean revisedup,
 		Boolean reviseddown,
-		String[] consensus) {
+		List<String> consensus) {
 
 	public ForecastFilterRequest(SubFilter upsidedownside, SubFilter estimatesnumber) {
-		this(upsidedownside, estimatesnumber, true, true, new String[0]);
+		this(upsidedownside, estimatesnumber, true, true, List.of());
 	}
 
 	public static final ForecastFilterRequest EMPTY =
-			new ForecastFilterRequest(SubFilter.EMPTY, SubFilter.EMPTY, true, true, new String[0]);
+			new ForecastFilterRequest(SubFilter.EMPTY, SubFilter.EMPTY, true, true, List.of());
 }
