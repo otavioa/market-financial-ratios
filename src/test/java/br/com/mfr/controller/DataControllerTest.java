18a932f45dd35f7609a5e42cd7c45d8669137ea0
otavioa <otavio.augusto.jr@gmail.com>
2024-07-21 01:36:30 -0300

    - Fix async intermittency from test

--- src/test/java/br/com/mfr/controller/DataControllerTest.java
+++ src/test/java/br/com/mfr/controller/DataControllerTest.java
@@ -15,6 +15,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -23,6 +24,7 @@ import static br.com.mfr.service.statusinvest.StatusInvestResources.*;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @MockMvcApp
@@ -45,7 +47,12 @@ class DataControllerTest {
         mockExternalUrlGet(STOCKS, newResponse(3L, "EMPRESA STOCKS", "SSS", 102.00));
         mockExternalUrlGet(REITS, newResponse(4L, "EMPRESA REITS", "RRR", 103.00));
 
-        performRequest(ApiEndpoints.DATA_POPULATE)
+
+        MvcResult mvcResult = performRequest(ApiEndpoints.DATA_POPULATE)
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        mvc.perform(asyncDispatch(mvcResult))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("text/event-stream"))
                 .andExpect(jsonPath("$", Matchers.allOf(
