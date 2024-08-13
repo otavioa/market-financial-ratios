aa5c9b6e344e8cd31638d3bb98ed53694a3020d8
otavioa <otavio.augusto.jr@gmail.com>
2024-08-13 01:09:11 -0300

    - Obligate to inform URL's

--- src/main/java/br/com/mfr/service/statusinvest/StatusInvestURLProperties.java
+++ src/main/java/br/com/mfr/service/statusinvest/StatusInvestURLProperties.java
@@ -1,8 +1,11 @@
 package br.com.mfr.service.statusinvest;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import static br.com.mfr.ConfigProperties.APP_EXTERNAL_PREFIX;
 
+@Validated
 @ConfigurationProperties(APP_EXTERNAL_PREFIX + ".statusinvest.url")
-public record StatusInvestURLProperties( String ticker, String advanced){ }
+public record StatusInvestURLProperties(@NotBlank String ticker, @NotBlank String advanced){ }
--- src/main/java/br/com/mfr/service/yahoo/YahooURLProperties.java
+++ src/main/java/br/com/mfr/service/yahoo/YahooURLProperties.java
@@ -1,8 +1,11 @@
 package br.com.mfr.service.yahoo;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import static br.com.mfr.ConfigProperties.APP_EXTERNAL_PREFIX;
 
+@Validated
 @ConfigurationProperties(APP_EXTERNAL_PREFIX + ".yahoo.url")
-public record YahooURLProperties(String cookies, String crumb, String etfScreener){ }
+public record YahooURLProperties(@NotBlank String cookies, @NotBlank String crumb, @NotBlank String etfScreener){ }
