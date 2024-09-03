673354df7af95bfed483edd9034790b625394844
otavioa <otavio.augusto.jr@gmail.com>
2024-09-03 02:47:13 -0300

    - Setting utils private (avoid sonar warning)

--- src/main/java/br/com/mfr/util/CollectionUtils.java
+++ src/main/java/br/com/mfr/util/CollectionUtils.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 
 public class CollectionUtils {
 
+    private CollectionUtils() { }
+
     public static boolean isEmpty(Collection collection) {
         return (collection == null || collection.isEmpty());
     }
